a5c51d7afc1f24d07230f037ca9acfa660f88e4b
Joana Rosa Nunes <jjnn.rosa19@gmail.com>
2025-06-03 19:07:30 -0300

    Alterção dos testes

--- test_app.py
+++ test_app.py
@@ -12,19 +12,21 @@ class APITestCase(unittest.TestCase):
         # Criação do cliente de teste
         cls.client = app.test_client()
 
-    def test_home(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
+    def test_rota_invalida(self):
+        response = self.client.get('/rota_invalida')
+        self.assertEqual(response.status_code, 404)
 
-    def test_login(self):
-        response = self.client.post('/login')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
 
-    def test_protected_no_token(self):
-        response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+    def test_get_items(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"items": ["item1", "item2", "item3"]})
+
+
+    def test_token_invalido(self):
+        headers = {'Authorization': 'Bearer tokeninvalido123'}
+        response = self.client.get('/protected', headers=headers)
+        self.assertEqual(response.status_code, 422) 
 
 if __name__ == '__main__':
     unittest.main()
